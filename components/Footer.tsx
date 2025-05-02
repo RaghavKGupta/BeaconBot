011c5f72f5d96802a394f24efc3b7eee5842ce11
Raghav Gupta <rgupta11@terpmail.umd.edu>
2025-05-02 13:34:41 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,6 @@ const Footer = () => {
   return (
     <footer className="chatbot-text-tertiary flex justify-between text-sm mt-6">
       <div className="ml-auto flex flex-row items-center">
-        <span className="mr-1">Georgia Institute of Technology - MGT 8803 - AI in Business</span>
-        <span className="mr-1">(Raghav Gupta, Nigam Wadhwa, Ruhma Mehek Khan)</span>
       </div>
     </footer>
   );
